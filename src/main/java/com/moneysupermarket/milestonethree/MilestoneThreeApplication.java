5480a89b32d050584a685cea75d0cd9495df394c
Daniel Fegan <daniel.fegan@moneysupermarket.com>
2018-10-01 18:48:28 +0100

    initial commit

---
+++ src/main/java/com/moneysupermarket/milestonethree/MilestoneThreeApplication.java
@@ -0,0 +1,12 @@
+package com.moneysupermarket.milestonethree;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MilestoneThreeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MilestoneThreeApplication.class, args);
+	}
+}
---
+++ src/test/java/com/moneysupermarket/milestonethree/MilestoneThreeApplicationTests.java
@@ -0,0 +1,16 @@
+package com.moneysupermarket.milestonethree;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class MilestoneThreeApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
