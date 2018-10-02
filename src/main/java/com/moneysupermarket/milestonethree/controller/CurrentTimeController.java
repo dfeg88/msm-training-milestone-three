58bcf622c7767c7a1932f95697ccd02c5adb70ee
Daniel Fegan <daniel.fegan@moneysupermarket.com>
2018-10-02 08:32:21 +0100

    rename method and organise imports

--- src/main/java/com/moneysupermarket/milestonethree/controller/CurrentTimeController.java
+++ src/main/java/com/moneysupermarket/milestonethree/controller/CurrentTimeController.java
@@ -1,19 +1,17 @@
 package com.moneysupermarket.milestonethree.controller;
 
 import com.moneysupermarket.milestonethree.model.CurrentTime;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.time.LocalDateTime;
 import java.time.ZoneId;
 
-import static org.springframework.web.bind.annotation.RequestMethod.GET;
-
 @RestController
 public class CurrentTimeController {
 
-    @RequestMapping(value = "/date-times", method = GET)
-    public CurrentTime getCurrentTime() {
+    @GetMapping("/date-times")
+    public CurrentTime getLocalAndCanadianDateTimes() {
         return new CurrentTime(
                 LocalDateTime.now(),
                 LocalDateTime.now(ZoneId.of("UTC-07:00"))
