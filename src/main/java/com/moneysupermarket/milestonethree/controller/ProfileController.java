4da7fe9326caf6f5de30525d40d7c1db61017e02
Daniel Fegan <daniel.fegan@moneysupermarket.com>
2018-10-04 13:00:14 +0100

    replace explicit constructor with lombok annotation

--- src/main/java/com/moneysupermarket/milestonethree/controller/ProfileController.java
+++ src/main/java/com/moneysupermarket/milestonethree/controller/ProfileController.java
@@ -3,7 +3,7 @@ package com.moneysupermarket.milestonethree.controller;
 import com.moneysupermarket.milestonethree.model.Profile;
 import com.moneysupermarket.milestonethree.model.ProfileResponse;
 import com.moneysupermarket.milestonethree.repository.ProfileRepository;
-import org.springframework.beans.factory.annotation.Autowired;
+import lombok.AllArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -12,15 +12,11 @@ import org.springframework.web.bind.annotation.RestController;
 
 import java.util.UUID;
 
+@AllArgsConstructor
 @RestController
 public class ProfileController {
 
     private final ProfileRepository profileRepository;
-
-    @Autowired
-    public ProfileController(ProfileRepository profileRepository) {
-        this.profileRepository = profileRepository;
-    }
 
     @PostMapping(value = "profile")
     public ResponseEntity<ProfileResponse> save(@RequestBody Profile profile) {
